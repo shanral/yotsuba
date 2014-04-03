c359bbc72a194edcdbed19e65706f849f0cd9460
Andrew Loucky <andrew.loucky@gmail.com>
2014-04-02 18:02:06 -0700

    turn extract into method that requires the initialization of tw less vars

--- parse_landfills.rb
+++ parse_landfills.rb
@@ -1,44 +1,39 @@
 #!/usr/bin/env ruby
 
-def write_and_reassign
-  if @local_county != @county
+def write_and_reassign(local_county, local_state)
+  if local_county != @county
     @out_file.write "#{@county},#{@state},#{@num_dump}\n" if @county != ''
-     @county = @local_county
-     @state  = @local_state
+     @county = local_county
+     @state  = local_state
      @num_dump = 0
   end
 
   @num_dump += 1
 end
 
+# this text file was generated with ps2pdf from the MSW pdf original
 file = open('MSW_sites.txt')
 @out_file = File.new('output.txt', 'w')
 @out_file.write "county,state,num_dump\n"
 
+# variable initialization
 @num_dump = 0
 @county, @state = ''
-@local_county, @local_state = ''
-
 
 file.each_line do |line|
   chunks = line.chomp.split(/\s{2,}/)
+
+  # conditionals to account for various types of formatting in the original
   if (chunks.length == 5 && chunks[0] =~ /\s?\d+/)
     if chunks[4] =~ /\d+/
-      @local_county = chunks[2]
-      @local_state  = chunks[3]
+      write_and_reassign(chunks[2], chunks[3])
     else
-      @local_county = chunks[3]
-      @local_state  = chunks[4]
+      write_and_reassign(chunks[3], chunks[4])
     end
-    write_and_reassign
   elsif (chunks.length == 6 && chunks[0] =~ /\s?\d+/)
-    @local_county = chunks[3]
-    @local_state  = chunks[4]
-    write_and_reassign
+    write_and_reassign(chunks[3], chunks[4])
   elsif (chunks.length == 4 || chunks.length == 3) && chunks [0] =~ /\s?\d+/
-    @local_county = chunks[-2]
-    @local_state  = chunks[-1]
-    write_and_reassign
+    write_and_reassign(chunks[-2], chunks[-1])
   else
     # Garbage
   end
