7e067e09b006b1138486369fb2e743022fe17429
Andrew Loucky <andrew.loucky@gmail.com>
2014-04-02 17:34:25 -0700

    extract repeated code extract

--- parse_landfills.rb
+++ parse_landfills.rb
@@ -1,11 +1,22 @@
 #!/usr/bin/env ruby
 
+def write_and_reassign
+  if @local_county != @county
+    @out_file.write "#{@county},#{@state},#{@num_dump}\n" if @county != ''
+     @county = @local_county
+     @state  = @local_state
+     @num_dump = 0
+  end
+
+  @num_dump += 1
+end
+
 file = open('MSW_sites.txt')
 @out_file = File.new('output.txt', 'w')
 @out_file.write "county,state,num_dump\n"
 
 @num_dump = 0
-county, state = ''
+@county, @state = ''
 @local_county, @local_state = ''
 
 
@@ -19,41 +30,15 @@ file.each_line do |line|
       @local_county = chunks[3]
       @local_state  = chunks[4]
     end
-
-    if @local_county != county
-      @out_file.write "#{county},#{state},#{@num_dump}\n" if county != ''
-      county = @local_county
-      state  = @local_state
-      @num_dump = 0
-    end
-
-    @num_dump += 1
-
+    write_and_reassign
   elsif (chunks.length == 6 && chunks[0] =~ /\s?\d+/)
     @local_county = chunks[3]
     @local_state  = chunks[4]
-
-    if @local_county != county
-      @out_file.write "#{county},#{state},#{@num_dump}\n" if county != ''
-      county = @local_county
-      state  = @local_state
-      @num_dump = 0
-    end
-
-    @num_dump += 1
-
+    write_and_reassign
   elsif (chunks.length == 4 || chunks.length == 3) && chunks [0] =~ /\s?\d+/
     @local_county = chunks[-2]
     @local_state  = chunks[-1]
-
-    if @local_county != county
-      @out_file.write "#{county},#{state},#{@num_dump}\n" if county != ''
-      county = @local_county
-      state  = @local_state
-      @num_dump = 0
-    end
-
-    @num_dump += 1
+    write_and_reassign
   else
     # Garbage
   end
